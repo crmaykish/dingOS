055c65e211baa6777eb09ecf46072c66f5e03752
crmaykish <crmaykish@gmail.com>
2017-04-19 19:40:24 -0400

    Writing to the framebuffer from C

--- kmain.c
+++ kmain.c
@@ -1,3 +1,20 @@
-int sum_of_three(int arg1, int arg2, int arg3) {
-    return arg1 + arg2 + arg3;
+#define FB_GREEN     2
+#define FB_DARK_GREY 8
+
+char *fb = (char *) 0x000B8000;
+
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg) {
+    fb[i] = c;
+    fb[i+1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+}
+
+void fb_clear() {
+    int i;
+    for (i = 0; i < 80 * 25; i++) {
+        fb_write_cell(i * 2, ' ', FB_GREEN, FB_DARK_GREY);
+    }
+}
+
+void kmain() {
+    fb_clear();
 }
--- loader.s
+++ loader.s
@@ -6,13 +6,21 @@ CHECKSUM        equ -MAGIC_NUMBER
 
 KERNEL_STACK_SIZE equ 4096
 
-section .text:
+section .bss
+align 4
+kernel_stack:
+    resb KERNEL_STACK_SIZE
+
+section .text
 align 4
     dd MAGIC_NUMBER
     dd FLAGS
     dd CHECKSUM
 
-loader:
-    mov eax, 0xDEADBEEF
+    mov esp, kernel_stack + KERNEL_STACK_SIZE
+
+    extern kmain
+    call kmain
+
 .loop:
     jmp .loop
