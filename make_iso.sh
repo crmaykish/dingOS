395ff6558645b0cc3601f1931f7a3f89f04c3526
crmaykish <crmaykish@gmail.com>
2017-04-19 17:36:02 -0400

    Updated build script

--- make_iso.sh
+++ make_iso.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-genisoimage -R                              \
+
+echo "Compiling loader..."
+nasm -f elf32 loader.s
+
+echo "Linking executable..."
+ld -T link.ld -melf_i386 loader.o -o kernel.elf
+
+echo "Downloading GRUB..."
+wget http://littleosbook.github.com/files/stage2_eltorito
+
+echo "Copying kernel and GRUB to ISO folder..."
+cp stage2_eltorito iso/boot/grub/
+cp kernel.elf iso/boot/
+
+echo "Creating ISO..."
+genisoimage -R                              	\
                 -b boot/grub/stage2_eltorito    \
                 -no-emul-boot                   \
                 -boot-load-size 4               \
@@ -9,3 +24,5 @@ genisoimage -R                              \
                 -boot-info-table                \
                 -o os.iso                       \
                 iso
+
+echo "All done!"
