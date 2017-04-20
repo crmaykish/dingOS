809e40187bd2b2476875d2b9e9b3379c39676804
crmaykish <crmaykish@gmail.com>
2017-04-19 20:41:13 -0400

    Added screenbuffer write interface

--- kmain.c
+++ kmain.c
@@ -16,13 +16,6 @@ void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg) {
     fb[i+1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
 }
 
-void fb_clear() {
-    int i;
-    for (i = 0; i < 80 * 25; i++) {
-        fb_write_cell(i * 2, ' ', FB_GREEN, FB_DARK_GREY);
-    }
-}
-
 void fb_move_cursor(unsigned short pos) {
     outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
     outb(FB_DATA_PORT, ((pos >> 8) & 0x00FF));
@@ -30,8 +23,24 @@ void fb_move_cursor(unsigned short pos) {
     outb(FB_DATA_PORT, pos & 0x00FF);
 }
 
+void fb_clear() {
+    int i;
+    for (i = 0; i < 80 * 25; i++) {
+        fb_write_cell(i * 2, ' ', FB_GREEN, FB_DARK_GREY);
+    }
+    
+    fb_move_cursor(0);
+}
+
+void write(char *buf, unsigned int len) {
+    unsigned int i;
+    for (i = 0; i < len; i++) {
+        fb_write_cell(i * 2, buf[i], FB_GREEN, FB_DARK_GREY);
+    }
+}
+
 void kmain() {
     fb_clear();
-
+    write("Welcome To dingOS v0.0.1", 24);
     fb_move_cursor(80);
 }
